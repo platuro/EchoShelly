8bcf5a2efb98e112d436bb4955cde0fa755a0807
Platuro <30547642+platuro@users.noreply.github.com>
2024-06-02 18:33:27 +0200

    update

---
+++ config.py
@@ -0,0 +1,10 @@
+from ShellyClient import ShellyClient
+
+email = "xxxx@xxxx.com"
+password = "xxxxxxxxx"
+serial_number = "1234567890"
+
+shelly_clients = [
+    ShellyClient("192.168.1.1", "", ""),
+    ShellyClient("192.168.1.1", "", "")
+]
